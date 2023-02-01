a16473f6c6c97938f867898d94b5056122481228
Suriyaa <suriyaaap13@gmail.com>
2023-02-01 23:03:27 +0530

    Apply CreateContext feature in the app to pass frequently passed data

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App(){
     const deleteBookById = async (id)=>{
         await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter((book)=>{
-            return book.id!=id;
+            return book.id!==id;
         });
         setBooks(updatedBooks);
     }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,17 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import React from 'react';
 import BookShow from './BookShow';
 
 export default function BookList({books, deleteBookById, onEdit}) {
+  const { count, incrementCount } = useContext(BooksContext);
   const renderedBooks = books.map((book)=>{
     return <BookShow key = {book.id} book = {book} deleteBookById = {deleteBookById} onEdit = {onEdit}/>;
   })
   return (
     <div className='book-list'>
+        {count}
+        <button onClick={incrementCount}>click</button>
         {renderedBooks}
     </div>
   )
